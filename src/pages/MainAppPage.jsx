149db1d11ae72c4ba493fc69ccbfbd19250f64cd
milenatarkowska <milenatarkowska1@gmail.com>
2025-08-12 13:10:02 +0200

    Added elements to main page header

--- src/pages/MainAppPage.jsx
+++ src/pages/MainAppPage.jsx
@@ -1,3 +1,20 @@
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+import { useState } from 'react';
+
 export default function MainApp() {
-    return <h1>Main app view</h1>;
+    const [visible, setVisible] = useState(false);
+    return(
+        <div className="main-app-page">
+            <div className="header">
+                <div className="card flex justify-content-center">
+                    <Sidebar visible={visible} onHide={() => setVisible(false)}>
+                        <h2>Your to-do lists</h2>
+                    </Sidebar>
+                    <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} />
+                </div>
+                <h1>To-Do List</h1>
+            </div>
+        </div>
+    );
 }
