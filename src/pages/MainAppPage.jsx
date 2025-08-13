b39d58072cc8ff8d4ee6e1463af43c7c31bac680
milenatarkowska <milenatarkowska1@gmail.com>
2025-08-13 12:54:50 +0200

    Styled main page background and header

--- src/pages/MainAppPage.jsx
+++ src/pages/MainAppPage.jsx
@@ -1,20 +1,21 @@
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
 import { useState } from 'react';
+import './MainAppPage.css';
 
 export default function MainApp() {
     const [visible, setVisible] = useState(false);
     return(
         <div className="main-app-page">
-            <div className="header">
-                <div className="card flex justify-content-center">
+            <header>
+                <div className="custom-sidebar">
                     <Sidebar visible={visible} onHide={() => setVisible(false)}>
                         <h2>Your to-do lists</h2>
                     </Sidebar>
                     <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} />
                 </div>
                 <h1>To-Do List</h1>
-            </div>
+            </header>
         </div>
     );
 }
